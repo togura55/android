76d46578ccc2c851e0a6b947f906fe7b0318cfae
togura55 <togura55@gmail.com>
2019-02-18 18:22:05 +0900

    WdC4A
    Updates

--- WdController/app/src/main/java/com/wacom/ogura/tsuyoshi/wdcontroller/MainActivity.java
+++ WdController/app/src/main/java/com/wacom/ogura/tsuyoshi/wdcontroller/MainActivity.java
@@ -1,43 +1,87 @@
 package com.wacom.ogura.tsuyoshi.wdcontroller;
 
-/*import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.widget.Toast;*/
-
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    // 定数
+    // Constants
     private static final int    REQUEST_ENABLEBLUETOOTH = 1; // Bluetooth機能の有効化要求時の識別コード
     private static final int    REQUEST_CONNECTDEVICE   = 2; // デバイス接続要求時の識別コード
+    private static final int    PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
     // メンバー変数
     private BluetoothAdapter mBluetoothAdapter;    // BluetoothAdapter : Bluetooth処理で必要
     private String        mDeviceAddress = "";    // デバイスアドレス
+    private BluetoothGatt mBluetoothGatt = null;    // Gattサービスの検索、キャラスタリスティックの読み書き
+
+    // GUIアイテム
+    private Button mButton_Connect;    // 接続ボタン
+    private Button mButton_Disconnect;    // 切断ボタン
+
+    // BluetoothGattコールバック
+    private final BluetoothGattCallback mGattcallback = new BluetoothGattCallback()
+    {
+        // 接続状態変更（connectGatt()の結果として呼ばれる。）
+        @Override
+        public void onConnectionStateChange( BluetoothGatt gatt, int status, int newState )
+        {
+            if( BluetoothGatt.GATT_SUCCESS != status )
+            {
+                return;
+            }
+
+            if( BluetoothProfile.STATE_CONNECTED == newState )
+            {    // 接続完了
+                runOnUiThread( new Runnable()
+                {
+                    public void run()
+                    {
+                        // GUIアイテムの有効無効の設定
+                        // 切断ボタンを有効にする
+                        mButton_Disconnect.setEnabled( true );
+                    }
+                } );
+                return;
+            }
+            if( BluetoothProfile.STATE_DISCONNECTED == newState )
+            {    // 切断完了（接続可能範囲から外れて切断された）
+                // 接続可能範囲に入ったら自動接続するために、mBluetoothGatt.connect()を呼び出す。
+                mBluetoothGatt.connect();
+                return;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // GUIアイテム
+        mButton_Connect = (Button)findViewById( R.id.button_connect );
+        mButton_Connect.setOnClickListener( this );
+        mButton_Disconnect = (Button)findViewById( R.id.button_disconnect );
+        mButton_Disconnect.setOnClickListener( this );
 
         // Android端末がBLEをサポートしてるかの確認
         if( !getPackageManager().hasSystemFeature( PackageManager.FEATURE_BLUETOOTH_LE ) )
@@ -45,6 +89,15 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText( this, R.string.ble_is_not_supported, Toast.LENGTH_SHORT ).show();
             finish();    // アプリ終了宣言
             return;
+        }
+
+        // Android M Permission check
+        // Resolving for the Android6.0(sdk>23) Bluetooth Scan problem
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+            }
         }
 
         // Bluetoothアダプタの取得
@@ -58,6 +111,20 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    Toast.makeText(this, "Bluetooth Scan need Permissions", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
     // 初回表示時、および、ポーズからの復帰時
     @Override
     protected void onResume()
@@ -66,6 +133,42 @@ public class MainActivity extends AppCompatActivity
 
         // Android端末のBluetooth機能の有効化要求
         requestBluetoothFeature();
+
+        // GUIアイテムの有効無効の設定
+        mButton_Connect.setEnabled( false );
+        mButton_Disconnect.setEnabled( false );
+
+        // デバイスアドレスが空でなければ、接続ボタンを有効にする。
+        if( !mDeviceAddress.equals( "" ) )
+        {
+            mButton_Connect.setEnabled( true );
+        }
+
+        // 接続ボタンを押す
+        mButton_Connect.callOnClick();
+    }
+
+    // 別のアクティビティ（か別のアプリ）に移行したことで、バックグラウンドに追いやられた時
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // 切断
+        disconnect();
+    }
+
+    // アクティビティの終了直前
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if( null != mBluetoothGatt )
+        {
+            mBluetoothGatt.close();
+            mBluetoothGatt = null;
+        }
     }
 
     // Android端末のBluetooth機能の有効化要求
@@ -80,23 +183,6 @@ public class MainActivity extends AppCompatActivity
         startActivityForResult( enableBtIntent, REQUEST_ENABLEBLUETOOTH );
     }
 
-/*    // 機能の有効化ダイアログの操作結果
-    @Override
-    protected void onActivityResult( int requestCode, int resultCode, Intent data )
-    {
-        switch( requestCode )
-        {
-            case REQUEST_ENABLEBLUETOOTH: // Bluetooth有効化要求
-                if( Activity.RESULT_CANCELED == resultCode )
-                {    // 有効にされなかった
-                    Toast.makeText( this, R.string.bluetooth_is_not_working, Toast.LENGTH_SHORT ).show();
-                    finish();    // アプリ終了宣言
-                    return;
-                }
-                break;
-        }
-        super.onActivityResult( requestCode, resultCode, data );
-    }*/
 
     // 機能の有効化ダイアログの操作結果
     @Override
@@ -153,4 +239,62 @@ public class MainActivity extends AppCompatActivity
         }
         return false;
     }
+
+    @Override
+    public void onClick( View v )
+    {
+        if( mButton_Connect.getId() == v.getId() )
+        {
+            mButton_Connect.setEnabled( false );    // 接続ボタンの無効化（連打対策）
+            connect();            // 接続
+            return;
+        }
+        if( mButton_Disconnect.getId() == v.getId() )
+        {
+            mButton_Disconnect.setEnabled( false );    // 切断ボタンの無効化（連打対策）
+            disconnect();            // 切断
+            return;
+        }
+    }
+
+    // 接続
+    private void connect()
+    {
+        if( mDeviceAddress.equals( "" ) )
+        {    // DeviceAddressが空の場合は処理しない
+            return;
+        }
+
+        if( null != mBluetoothGatt )
+        {    // mBluetoothGattがnullでないなら接続済みか、接続中。
+            return;
+        }
+
+        // 接続
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice( mDeviceAddress );
+        mBluetoothGatt = device.connectGatt( this, false, mGattcallback );
+    }
+
+    // 切断
+    private void disconnect()
+    {
+        if( null == mBluetoothGatt )
+        {
+            return;
+        }
+
+        // 切断
+        //   mBluetoothGatt.disconnect()ではなく、mBluetoothGatt.close()しオブジェクトを解放する。
+        //   理由：「ユーザーの意思による切断」と「接続範囲から外れた切断」を区別するため。
+        //   ①「ユーザーの意思による切断」は、mBluetoothGattオブジェクトを解放する。再接続は、オブジェクト構築から。
+        //   ②「接続可能範囲から外れた切断」は、内部処理でmBluetoothGatt.disconnect()処理が実施される。
+        //     切断時のコールバックでmBluetoothGatt.connect()を呼んでおくと、接続可能範囲に入ったら自動接続する。
+        mBluetoothGatt.close();
+        mBluetoothGatt = null;
+        // GUIアイテムの有効無効の設定
+        // 接続ボタンのみ有効にする
+        mButton_Connect.setEnabled( true );
+        mButton_Disconnect.setEnabled( false );
+    }
+
 }
